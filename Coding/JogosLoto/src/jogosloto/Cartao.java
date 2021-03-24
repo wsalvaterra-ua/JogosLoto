8dc75da40219d849891e6ae1fc0d3b813e072fb3
Wilian Salvaterra <wsalvaterra@ua.pt>
2021-03-24 22:15:21 +0000

    Objeto Cartao Terminado

--- Coding/JogosLoto/src/jogosloto/Cartao.java
+++ Coding/JogosLoto/src/jogosloto/Cartao.java
@@ -13,33 +13,54 @@ import java.util.Random;
  */
 public class Cartao {
     Slot_Numero [][] slot;
-    int[][] slots_disponiveis;
+    int[] slots_disponiveis;
     
     public Cartao() {
-        slot = new Slot_Numero[3][15];
+        slot = new Slot_Numero[3][9];
+        slots_disponiveis = new int[3];
+        
         construir_cartao();
     }
-    public  void construir_cartao(){
+
+    public Slot_Numero[][] getSlot() {
+        return slot;
+    }
+    
+    private void construir_cartao(){
         
         for(int i = 0 ; i<3; i++){
             int espacos_vazios_disponiveis = 4;
             int espacos_numeros_disponiveis = 5;
-            for( int j = 0 ; j<9; j++){
-                int resultRand =randomNum(0, 1);
+            for( int j = 0 ; j < 9; j++){
+                int resultRand =randomNum(0, 2);
                 
                 if(espacos_vazios_disponiveis> 0 &&  resultRand == 0 || espacos_numeros_disponiveis <1 && espacos_vazios_disponiveis> 0)
                     espacos_vazios_disponiveis--;
                 else{
-                    
+                    slot[i][j] = new Slot_Numero(randomNum( (j * 10 + ((j==8) ? 0:1 )) , (j * 10 + ((j==8) ? 10:9 ) )));
                     espacos_numeros_disponiveis--;
                 }
-               
             }
         }
         
         
     }
-    public static int randomNum(int min, int max) {
+    private boolean MarcarNumeroSorteado(int numeroSorteado){
+        for(int i = 0 ; i<3; i++){
+            for( int j = 0 ; j < 9; j++){
+                if(slot[i][j] != null)
+                    if(!slot[i][j].getMarcado() && slot[i][j].getNumero() == numeroSorteado ){
+                        slot[i][j].setMarcado(true);
+                        slots_disponiveis[i]--;
+                        if(slots_disponiveis[i]<1) System.out.println("Aviso: Linha "+ (i+1) + " está completa!");
+                        return true;
+                    } 
+            }
+        }
+        return false;
+    }
+    
+    private static int randomNum(int min, int max) {
         Random r = new Random();
         return r.nextInt((max - min) + 1) + min;
     }
