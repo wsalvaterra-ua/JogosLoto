f90f6b11b71cf1ca55b5357b8b286de65312e471
Wilian Salvaterra <wsalvaterra@ua.pt>
2021-03-24 20:54:09 +0000

    Updt Cartao

--- Coding/JogosLoto/src/jogosloto/Cartao.java
+++ Coding/JogosLoto/src/jogosloto/Cartao.java
@@ -5,15 +5,42 @@
  */
 package jogosloto;
 
+import java.util.Random;
+
 /**
  *
  * @author bilss
  */
 public class Cartao {
-    int[][] numeros;
-    int[][] numeros_usados;
+    Slot_Numero [][] slot;
+    int[][] slots_disponiveis;
     
-    public static void main(String[] args) {
+    public Cartao() {
+        slot = new Slot_Numero[3][15];
+        construir_cartao();
+    }
+    public  void construir_cartao(){
         
+        for(int i = 0 ; i<3; i++){
+            int espacos_vazios_disponiveis = 4;
+            int espacos_numeros_disponiveis = 5;
+            for( int j = 0 ; j<9; j++){
+                int resultRand =randomNum(0, 1);
+                
+                if(espacos_vazios_disponiveis> 0 &&  resultRand == 0 || espacos_numeros_disponiveis <1 && espacos_vazios_disponiveis> 0)
+                    espacos_vazios_disponiveis--;
+                else{
+                    
+                    espacos_numeros_disponiveis--;
+                }
+               
+            }
+        }
+        
+        
+    }
+    public static int randomNum(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
     }
 }
