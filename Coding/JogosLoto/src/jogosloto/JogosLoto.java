0fd5dcf062e82ac017286fa846effe6e7c9c6420
Rui Oliveira <ruioliveira3@ua.pt>
2021-03-29 10:26:00 +0100

    Update JogosLoto.java

--- Coding/JogosLoto/src/jogosloto/JogosLoto.java
+++ Coding/JogosLoto/src/jogosloto/JogosLoto.java
@@ -15,11 +15,32 @@ public class JogosLoto {
 
     Scanner sc;
     
+    //menu inicial com as opções para o utilizador
     public void display_menuInicial(){
         System.out.println("*************************** Jogo Do Loto ***************************");
-        System.out.println("O seu cartão é este: ");
+        System.out.println("Escolha a opção seguinte: ");
+        System.out.println("1-Ver o seu cartão.");
+        System.out.println("2-Sortear Número.");
+        System.out.println("3-Exit.");
+        Scanner sc = new Scanner(System.in);
+        
+        switch (sc.nextInt()){
+            case 1:
+                //cartao.construir_cartao();
+                //System.out.println(cartao);
+            case 2:
+                //cartao.receber_numero_sorteado();
+                //cartao.getSlots_disponiveis();
+                question();
+            case 3:
+                break;
+            default:
+                System.out.println("Número Inválido.");
+                display_menuInicial();               
+        }
     }
     
+    //menu para após o jogo terminar
     public void question(){
         System.out.println("Terminou o Jogo, Parabéns!");
         System.out.println("Escolha a opção seguinte: ");
@@ -43,9 +64,11 @@ public class JogosLoto {
                 question();
         }  
     }
-    public int receber_numero_sorteado(int numero){
+    
+    //utilizador introduz o número sorteado
+    public void receber_numero_sorteado(){
             System.out.println("Introduza o número sorteado: ");
-            numero = sc.nextInt();
-            return numero;
-    }    
+            int numero = sc.nextInt();
+            //cartao.MarcarNumeroSorteado(numero);
+    } 
 }
