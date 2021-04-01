1d79bcd597f2122d580e722f100757a09d998529
Rui Oliveira <ruioliveira3@ua.pt>
2021-04-01 17:10:29 +0100

    Update JogosLoto.java

--- Coding/JogosLoto/src/jogosloto/JogosLoto.java
+++ Coding/JogosLoto/src/jogosloto/JogosLoto.java
@@ -9,39 +9,66 @@ import java.util.Scanner;
 
 /**
  *
- * @author bil
+ * @author rui
  */
 public class JogosLoto {
 
     Scanner sc;
+//    Cartao cartao;
+    public static void main(String[] args) {
+            Cartao cartao = null;
+            display_menuInicial(cartao);      
+    }
     
-    //menu inicial com as opções para o utilizador
-    public void display_menuInicial(){
-        System.out.println("*************************** Jogo Do Loto ***************************");
-        System.out.println("Escolha a opção seguinte: ");
-        System.out.println("1-Ver o seu cartão.");
-        System.out.println("2-Sortear Número.");
-        System.out.println("3-Exit.");
-        Scanner sc = new Scanner(System.in);
+    public static void display_menuInicial(Cartao cartao){
+        if(cartao== null){
+            cartao = new Cartao();
+            System.out.println("Cartão criado com sucesso.");          
+        }
         
+        System.out.println("*************************** Jogo Do Loto ***************************");
+        System.out.println("1-Ver Cartão.");
+        System.out.println("2-Sortear um número.");
+        System.out.println("3-Sair.");
+        Scanner sc = new Scanner(System.in);
         switch (sc.nextInt()){
-            case 1:
-                //cartao.construir_cartao();
-                //System.out.println(cartao);
+            case 1: 
+                Slot_Numero [][] slot = cartao.getSlot();
+                for(int i = 0 ; i<3; i++){
+                    for( int j = 0 ; j < 9; j++){
+                        System.out.print("[");
+                        if(slot[i][j] != null){
+                            if(slot[i][j].getMarcado())
+                                System.out.print("\"" + slot[i][j].getNumero() + "\"");
+                            else System.out.printf("%02d",slot[i][j].getNumero());  
+                        }else System.out.print("  ");
+                        System.out.print("] ");  
+                    }
+                    System.out.println("");
+                }     
+                display_menuInicial(cartao);
+                break;
             case 2:
-                //cartao.receber_numero_sorteado();
-                //cartao.getSlots_disponiveis();
-                question();
+                int numero;
+                System.out.println("Introduza o número sorteado: ");
+                numero = sc.nextInt();
+                if(cartao.MarcarNumeroSorteado(numero)){
+                    System.out.println("Numero: " + numero + " foi sorteado.\n");
+                    if(cartao.getSlots_disponiveis()[0] <1 && cartao.getSlots_disponiveis()[1] <1 && cartao.getSlots_disponiveis()[2] <1)
+                        completed_card(cartao);
+                }
+                else  System.out.println("Numero: " + numero + " não foi sorteado.\n");
+                display_menuInicial(cartao);
+                break;
             case 3:
                 break;
             default:
                 System.out.println("Número Inválido.");
-                display_menuInicial();               
-        }
+                display_menuInicial(cartao);
+        } 
     }
     
-    //menu para após o jogo terminar
-    public void question(){
+    public static void completed_card(Cartao cartao){
         System.out.println("Terminou o Jogo, Parabéns!");
         System.out.println("Escolha a opção seguinte: ");
         System.out.println("1-Iniciar um novo jogo com um novo cartão.");
@@ -51,24 +78,17 @@ public class JogosLoto {
         
         switch (sc.nextInt()){
             case 1: 
-                //cartao.contruir_Cartao();
-                //cartao.receber_Numero_Sorteado();
-                question();
+                System.out.println("");
+                display_menuInicial(null);
+                break;
             case 2:
-                //cartao.receber_numero_sorteado();
-                question();
+                System.out.println("");
+                display_menuInicial(cartao);               
             case 3:
                 break;
             default:
                 System.out.println("Número Inválido.");
-                question();
+                completed_card(cartao);
         }  
     }
-    
-    //utilizador introduz o número sorteado
-    public void receber_numero_sorteado(){
-            System.out.println("Introduza o número sorteado: ");
-            int numero = sc.nextInt();
-            //cartao.MarcarNumeroSorteado(numero);
-    } 
 }
