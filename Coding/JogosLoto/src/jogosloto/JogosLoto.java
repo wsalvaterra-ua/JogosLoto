6ea77ed6baf24c987f74166d12ddcb1ac7b015b8
Rui Oliveira <ruioliveira3@ua.pt>
2021-04-01 17:13:48 +0100

    Update JogosLoto.java

--- Coding/JogosLoto/src/jogosloto/JogosLoto.java
+++ Coding/JogosLoto/src/jogosloto/JogosLoto.java
@@ -9,18 +9,66 @@ import java.util.Scanner;
 
 /**
  *
- * @author bil
+ * @author rui
  */
 public class JogosLoto {
 
     Scanner sc;
-    
-    public void display_menuInicial(){
-        System.out.println("*************************** Jogo Do Loto ***************************");
-        System.out.println("O seu cartão é este: ");
+//    Cartao cartao;
+    public static void main(String[] args) {
+            Cartao cartao = null;
+            display_menuInicial(cartao);       
     }
     
-    public void question(){
+    public static void display_menuInicial(Cartao cartao){
+        if(cartao== null){
+            cartao = new Cartao();
+            System.out.println("Cartão criado com sucesso.");           
+        }
+        
+        System.out.println("*************************** Jogo Do Loto ***************************");
+        System.out.println("1-Ver Cartão.");
+        System.out.println("2-Sortear um número.");
+        System.out.println("3-Sair.");
+        Scanner sc = new Scanner(System.in);
+        switch (sc.nextInt()){
+            case 1: 
+                Slot_Numero [][] slot = cartao.getSlot();
+                for(int i = 0 ; i<3; i++){
+                    for( int j = 0 ; j < 9; j++){
+                        System.out.print("[");
+                        if(slot[i][j] != null){
+                            if(slot[i][j].getMarcado())
+                                System.out.print("\"" + slot[i][j].getNumero() + "\"");
+                            else System.out.printf("%02d",slot[i][j].getNumero());  
+                        }else System.out.print("  ");
+                        System.out.print("] ");  
+                    }
+                    System.out.println("");
+                }     
+                display_menuInicial(cartao);
+                break;
+            case 2:
+                int numero;
+                System.out.println("Introduza o número sorteado: ");
+                numero = sc.nextInt();
+                if(cartao.MarcarNumeroSorteado(numero)){
+                    System.out.println("Numero: " + numero + " foi sorteado.\n");
+                    if(cartao.getSlots_disponiveis()[0] <1 && cartao.getSlots_disponiveis()[1] <1 && cartao.getSlots_disponiveis()[2] <1)
+                        completed_card(cartao);
+                }
+                else  System.out.println("Numero: " + numero + " não foi sorteado.\n");
+                display_menuInicial(cartao);
+                break;
+            case 3:
+                break;
+            default:
+                System.out.println("Número Inválido.");
+                display_menuInicial(cartao);
+        } 
+    }
+    
+    public static void completed_card(Cartao cartao){
         System.out.println("Terminou o Jogo, Parabéns!");
         System.out.println("Escolha a opção seguinte: ");
         System.out.println("1-Iniciar um novo jogo com um novo cartão.");
@@ -30,22 +78,17 @@ public class JogosLoto {
         
         switch (sc.nextInt()){
             case 1: 
-                //cartao.contruir_Cartao();
-                //cartao.receber_Numero_Sorteado();
-                question();
+                System.out.println("");
+                display_menuInicial(null);
+                break;
             case 2:
-                //cartao.receber_numero_sorteado();
-                question();
+                System.out.println("");
+                display_menuInicial(cartao);               
             case 3:
                 break;
             default:
                 System.out.println("Número Inválido.");
-                question();
+                completed_card(cartao);
         }  
     }
-    public int receber_numero_sorteado(int numero){
-            System.out.println("Introduza o número sorteado: ");
-            numero = sc.nextInt();
-            return numero;
-    }    
 }
