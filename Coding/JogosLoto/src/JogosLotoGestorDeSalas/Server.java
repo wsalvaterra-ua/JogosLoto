45808296c02944c045226a4279ce0ea3386f4b82
Rui Oliveira <ruioliveira3@ua.pt>
2021-06-04 22:45:42 +0100

    Update Server.java

--- Coding/JogosLoto/src/JogosLotoGestorDeSalas/Server.java
+++ Coding/JogosLoto/src/JogosLotoGestorDeSalas/Server.java
@@ -14,13 +14,15 @@ import java.util.*;
 import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
   
 // Server class
 public class Server implements Runnable
 {
     
     private boolean terminarJogo;
-    private boolean jogoIniciado;
+    protected boolean inscricaoDeJogadoresTerminou;
+
     private final GSalaGUI GestorGUI;
     private final HashMap<Integer,ServerCommunication> jogadoresSocket;
 
@@ -30,9 +32,12 @@ public class Server implements Runnable
         super();
         
         GestorGUI = gestorDeSala;
-        serverSocket = new ServerSocket(ServerCommunication.PORTA);
-        terminarJogo = false;
+        serverSocket = new ServerSocket();
+        serverSocket.setReuseAddress(true);
+        serverSocket.bind(new InetSocketAddress(ServerCommunication.PORTA));
 
+        terminarJogo = false;
+        inscricaoDeJogadoresTerminou  = false;
 
         jogadoresSocket = new HashMap<>();
     }
@@ -43,53 +48,84 @@ public class Server implements Runnable
         receberClientes();
         if(jogadoresSocket.size()<1){
             System.out.println("Nnenhum cliente ingressou se");
+            try {
+                this.serverSocket.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
             return;
         }   
         while(!terminarJogo){
-                    
-                   ArrayList<String[]> finalistas = new ArrayList<>();
-        
-        for(int i: jogadoresSocket.keySet()){
-            ServerCommunication cliente = jogadoresSocket.get(i);
-       
-            if(cliente.terminouCartao()){
-                try {
+          
 
-                    System.out.println("Classe Server Detetou que um cartao terminou");
-                    String numerosDecript = JogosLotoJogador.EncriptacaoAES.decrypt(cliente.cartaoNumerosEncoded, cliente.chaveDecriptar);
-                    if(numerosDecript == null)
-                        System.out.println("Chave de Encriptacao Incorreta");
-                    else{
-                        finalistas.add(new String[]{Integer.toString(cliente.jogadorID),numerosDecript});
-                        GestorGUI.estado_BotaoSortear(false);
-                    //tempo para que os outros clientes se atualizem caso queiram se declarar como vencedores também
-                        Thread.sleep(3000);
+            ArrayList<String[]> finalistas = new ArrayList<>();
+           Set<Integer> jogadores_IT = jogadoresSocket.keySet();
+            for(int i: jogadores_IT){
+                ServerCommunication cliente = jogadoresSocket.get(i);
+                
+                if(cliente.terminouCartao() && cliente.terminarJogo == false){
+                    try {
+
+    
+                        String numerosDecript = JogosLotoJogador.EncriptacaoAES.decrypt(cliente.getCartaoNumerosEncoded(), cliente.getChaveDecriptar());
+                        if(numerosDecript == null)
+                            System.out.println("Chave de Encriptacao Incorreta");
+                        else{
+                            finalistas.add(new String[]{Integer.toString(cliente.getJogadorID()),numerosDecript});
+                            GestorGUI.estado_BotaoSortear(false);
+                        //tempo para que os outros clientes se atualizem caso queiram se declarar como vencedores também
+                            Thread.sleep(3000);
+                        }
+                    } catch (InterruptedException ex) {
+                            for(int b: jogadoresSocket.keySet())
+                                jogadoresSocket.get(b).terminarJogo = true;
+                            this.terminarJogo = true;
+                            System.out.println("temrinou jogo durante o timer checar clientes");
                     }
-                } catch (InterruptedException ex) {
-                        for(int b: jogadoresSocket.keySet())
-                            jogadoresSocket.get(b).terminarJogo = true;
-                        this.terminarJogo = true;
-                        System.out.println("temrinou jogo durante o timer checar clientes");
-                }
-
+                    System.out.println("finallistas:" + finalistas.size());
+               }
+                if(cliente.terminarJogo)
+                    jogadoresSocket.remove(i);
+                
+                    
             }
-        }
-            System.out.println("finallistas:" + finalistas.size());
-        if(finalistas.size() > 0)
-            if(this.GestorGUI.finalizarJogo(finalistas)){
-                this.terminarJogo = true;
+
+            if(finalistas.size() > 0)
+                if(this.GestorGUI.finalizarJogo(finalistas)){
+                    for(int b: jogadoresSocket.keySet())
+                        jogadoresSocket.get(b).terminarJogo = true;
+                    this.terminarJogo = true;
+                     break;
+                }else{
+
+                    String mensagem = "O(s) jogador(es) ";
+                    for( ServerCommunication jogador : this.jogadoresSocket.values())
+                        mensagem+= jogador.getNomeJogador() + ", ";
+                    mensagem+=" tentaram terminar o jogo mas não têm números sorteados suficientes no cartão para o fazer";
+                    System.out.println(mensagem);
+                    GestorGUI.estado_BotaoSortear(true);
+                }
+            
+            //sleep para ciclo nao trabalhar excessivamente
+            try {
+                Thread.sleep(ServerCommunication.INTERVALO_ATUALIZACAO);
+            } catch (InterruptedException ex) {
                 for(int b: jogadoresSocket.keySet())
                     jogadoresSocket.get(b).terminarJogo = true;
-                 break;
-            }else{
-                GestorGUI.estado_BotaoSortear(true);
+                this.terminarJogo = true;
             }
+        }
+        this.enviarMSG("jogoTerminou->true");
+        System.out.println("terminou");
+        for(int b: jogadoresSocket.keySet())
+            jogadoresSocket.get(b).terminarJogo = true;
+
         try {
-            Thread.sleep(200);
-        } catch (InterruptedException ex) {
+            this.serverSocket.close();
+        } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
-        }
+   
    }
 
 
@@ -108,23 +144,22 @@ public class Server implements Runnable
             System.out.println("nome vazio?");
             return null;
         }
-        return this.jogadoresSocket.get(jogadorID).nomeJogador;
+        return this.jogadoresSocket.get(jogadorID).getNomeJogador();
         
     }
     
 
     public void receberClientes() {
 // referencia: https://www.baeldung.com/java-measure-elapsed-time
-        long start = System.currentTimeMillis();
 
-        long finish = System.currentTimeMillis(); 
             try {
-                serverSocket.setSoTimeout(12000);
+                serverSocket.setSoTimeout(400);
             } catch (SocketException ex) {
-                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+               
+                JOptionPane.showMessageDialog(this.GestorGUI,  "Ocorreu um erro, por favor inicie um novo jogo!","Erro!",javax.swing.JOptionPane.ERROR_MESSAGE); 
+                return;
             }
-            while( finish - start < 12000){
-                System.out.println("Tempo restante antes de iniciar" +   (finish - start));
+            while( !inscricaoDeJogadoresTerminou){
                 try {
 
                     Socket socket;
@@ -132,7 +167,7 @@ public class Server implements Runnable
                     System.out.println("a espera" );
 
                     socket = serverSocket.accept();
-                    if( finish - start > 12000) break;
+                    if(inscricaoDeJogadoresTerminou) break;
 
                     int jogadorID = jogadoresSocket.size()+1;
                     ServerCommunication  cliente_novo = new ServerCommunication(socket, jogadorID, GestorGUI);
@@ -144,120 +179,22 @@ public class Server implements Runnable
 
 
                 } catch (IOException ex) {
-                    System.out.println("Erro ao aceitar novo cliente, mas o programa continua");
+                    
                 }
-                finish = System.currentTimeMillis(); 
+         
             }
+            
             System.out.println("Parou de aceitar novas conexoes");
             
  
             
     }
 
+    public void setTerminarJogo(boolean tterminarJogo) {
+        this.terminarJogo = tterminarJogo;
+    }
+
  
 
 
-}
-  
-// ClientHandler class
-class ClientHandler extends Thread 
-{
-    DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd");
-    DateFormat fortime = new SimpleDateFormat("hh:mm:ss");
-    final DataInputStream dis;
-    final DataOutputStream dos;
-    final Socket s;
-    PrintWriter saida;
-      
-  
-    // Constructor
-    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos) 
-    {
-        this.s = s;
-        this.dis = dis;
-        this.dos = dos;
-        saida  = new PrintWriter(dos, true);
-    }
-  
-    @Override
-    public void run() 
-    {
-        String received;
-        String toreturn;
-        Integer vp = 0;
-        while (true) 
-        {
-            try {
-                
-                if(vp == 0){
-                    saida.println("numIdentificacao:102");
-                    System.out.println("enviou num id");
-                }
-                
-                
-                vp++;
-                if(vp !=0){
-                  
-                // receive the answer from client
-                if(vp == 3){
-                    try {
-                        sleep(3000);
-                        saida.println("jogoTerminou:true;");
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    
-                }
-                received = dis.readLine();
-                System.out.println(received);
-                  
-                if(received.equals("Exit"))
-                { 
-                    System.out.println("Client " + this.s + " sends exit...");
-                    System.out.println("Closing this connection.");
-                    this.s.close();
-                    System.out.println("Connection closed");
-                    break;
-                }
-                  
-                // creating Date object
-                Date date = new Date();
-                  
-                // write on output stream based on the
-                // answer from the client
-                switch (received) {
-                  
-                    case "Date" :
-                        toreturn = fordate.format(date);
-                        dos.writeUTF(toreturn);
-                        break;
-                          
-                    case "Time" :
-                        toreturn = fortime.format(date);
-                        dos.writeUTF(toreturn);
-                        break;
-                          
-                    default:
-                        saida.println("jogoIniciado:true;");
-                        System.out.println("respondeu");
-                        break;
-                }
-            }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-          
-        try
-        {
-            // closing resources
-            this.dis.close();
-            this.dos.close();
-              
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-        
-        
-    }
 }
