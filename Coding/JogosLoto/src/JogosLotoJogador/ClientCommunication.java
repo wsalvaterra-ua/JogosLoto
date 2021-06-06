ecaefdd02cdcb00f02ac96825d1a75c5cd5917e4
Rui Oliveira <ruioliveira3@ua.pt>
2021-06-06 11:49:27 +0100

    Update ClientCommunication.java

--- Coding/JogosLoto/src/JogosLotoJogador/ClientCommunication.java
+++ Coding/JogosLoto/src/JogosLotoJogador/ClientCommunication.java
@@ -5,6 +5,7 @@
  */
 package JogosLotoJogador;
 
+import JogosLotoGestorDeSalas.ModalGameScores;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -14,18 +15,27 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.swing.JDialog;
+
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author bil
  */
-public class ClientCommunication extends SocketCommunicationStruct implements ActionListener{
-    
-    public ClientCommunication(){
+public class ClientCommunication extends SocketCommunicationStruct{
+    private boolean temErro;
+    private boolean terminouJogo; 
+    //compartilhar idJogador
+    private final JogadorGUI GUIJogo;
+    public String chave;
+    public int numIdentificacao;
+    public ClientCommunication(JogadorGUI GUIJogo){
         super();
         MSGEntrada = new ArrayList<>();
+        temErro = false;
+        terminouJogo = false;
+        this.GUIJogo = GUIJogo;
     }
     public  boolean conectar() {
         try {
@@ -38,22 +48,81 @@ public class ClientCommunication extends SocketCommunicationStruct implements Ac
         }
     }
     
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        try {
-            while(entrada.ready()){
-                String texto = entrada.readLine();
-                if(texto != null)
-                    MSGEntrada.add(texto);
-            }
-        } catch (IOException ex) {
-            
-        } 
-    } 
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            while(!temErro && !terminouJogo){
+                String inpt_ = entrada.readLine();
+                if(inpt_ != null){
+                    HashMap<String,String> dados = ClientCommunication.decodificar(inpt_);
+
+                    if(dados.containsKey("numeroSorteado"))
+                        try {
+                            this.GUIJogo.sortearNumero(Integer.parseInt(dados.get("numeroSorteado")));
+                        } catch (NumberFormatException n) {
+                        }
+                    
+                    
+                    
+                    if(dados.containsKey("jogoTerminou")){
+                        String nomeDoJogador = null;
+                        HashMap<String, Double> vencedores = new HashMap<>();
+                        System.out.println("dados de jogoterminou:" + dados.keySet());
+                        if(dados.get("jogoTerminou").equals("true")){
+                            this.terminouJogo = true;
+                            //estrutura de dado de vencedor a receber: ID,recompensa,Nome
+                            if(dados.containsKey("vencedores"))
+                                for(String vencedorIT : dados.get("vencedores").split(";")){
+                                    String[] vencedor = vencedorIT.split(",");
+                                    if(vencedor.length == 3)
+                                        try {
+                                            if(vencedor[2].length()<1)
+                                                throw new NumberFormatException();
+                                            vencedores.put(vencedor[2], Double.parseDouble(vencedor[1]));
+                                            if( Integer.valueOf(vencedor[0]) == this.numIdentificacao )
+                                                nomeDoJogador = vencedor[2];
+                                        } catch (NumberFormatException e) {
+                                        }
+                                }
+                            
+                            ModalGameScores myDialog = new ModalGameScores(GUIJogo, true,vencedores , nomeDoJogador);
+                            myDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+                            myDialog.setLocationRelativeTo(GUIJogo);  
+                            myDialog.setVisible(true);
+                            
+                            switch(myDialog.acao){
+                                case 0:
+                                    GUIJogo.resetarNumeros();
+                                    
+                                    break;
+                                case 1:
+                                    GUIJogo.novoJogo();
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    GUIJogo.dispose();
+                                    break;
+                                default:
+                                    break;
+                                
+                            }
+                      
+                        }
+                    } 
+                }     
+                Thread.sleep(ClientCommunication.INTERVALO_ATUALIZACAO);
+            }
+        } catch (IOException | InterruptedException ex) {
+            temErro = true;
+        } 
+        if(temErro){
+            JOptionPane.showMessageDialog(GUIJogo,"Houve um erro de conexão! Jogo será recomeçado!","Verifique os dados",javax.swing.JOptionPane.WARNING_MESSAGE);
+            GUIJogo.novoJogo();
+        }
     }
+
+
 
 }
